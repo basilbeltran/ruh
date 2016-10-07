3027ced946ff1e35b967f7e6112273003d11510e
Basil <Basil.beltran@gmail.com>
2016-10-07 12:19:58 -0600

    implement front end routing

--- work/js/ruh_module.js
+++ work/js/ruh_module.js
@@ -1,21 +1,61 @@
 'use strict';
 
-angular.module("RuhApp", []);
+angular.module("RuhApp", ['ngRoute']);
 //factories allow controllers to share data
 
 angular.module("RuhApp")
+    .controller('RuhMainController', mainController)
     .controller('RuhLoginController', loginController)
+    .controller('RuhProfileController', profileController)
     .factory('RuhUserFactory', userFactory)
+
+
+angular.module('RuhApp')
+        .config(myRouter);
+myRouter.$inject = ['$routeProvider'];
+function myRouter($routeProvider) {
+
+  $routeProvider
+  .when('/', { templateUrl: '/templates/login.html', controller: "RuhLoginController as loginMain" })
+  .when('/expert', { templateUrl: '/templates/expert.html', controller: "RuhExpertController as expertMain" })
+  .when('/question', { templateUrl: '/templates/question.html', controller: "RuhQuestionController as questionMain" })
+  .when('/profile', { templateUrl: '/templates/profile.html', controller: "RuhProfileController as profileMain" })
+  .otherwise({ redirectTo: '/'})
+}
+
+function mainController(){
+  var mainMain = this;
+  mainMain.titleLink = "RU Stuck"
+  mainMain.profileLink = "Profile"
+  mainMain.questionLink = "I'm Stuck"
+  mainMain.expertLink = "I'll Help"
+}
 
 function loginController(){
       var loginMain = this;
+      
+      loginMain.mainText = "Sign in to RU Stuck";
+      loginMain.subText = "Enter your email address and password.";
+      loginMain.keepText = "Keep me signed in";
+      loginMain.forgotText = "I forgot my password";
+      loginMain.emailText = "you@domain.com";
+      loginMain.passwordText = "password";
 
-      loginMain.ngBlur = function () {
-          loginMain.isBlur = true;
-          loginMain.blur = "Ng-Blur True";
+      loginMain.login = function(){
+        console.log('logged in maybe');
+      }
+}
+
+function profileController(){
+      var profileMain = this;
+
+      profileMain.changePassword = function(){
+        console.log('password changed');
       }
 
 }
+
+
 
 
   function userFactory(){
